91489a39fb3d13a0d40f70acc20c8f48b11c1c4f
Patrick Minogue <patrick.minogue@gmail.com>
2018-05-30 13:16:25 +0200

    Removed OnInit from SkinConsultationComponent

--- lib/skin_consultation_component/skin_consultation_component.dart
+++ lib/skin_consultation_component/skin_consultation_component.dart
@@ -1,14 +1,13 @@
 import 'package:angular/angular.dart';
+import 'package:bokain_consultation/bokain_consultation.dart';
 
 @Component(
   selector: 'skin-consultation-component',
   templateUrl: 'skin_consultation_component.html',
-  styleUrls: const ['skin_consultation_component.css'])
-class SkinConsultationComponent implements OnInit {
+  styleUrls: const ['skin_consultation_component.css'],
+  directives: const [],
+  providers: const [])
+class SkinConsultationComponent {
 
   SkinConsultationComponent();
-
-  @override
-  void ngOnInit() {}
-
 }
