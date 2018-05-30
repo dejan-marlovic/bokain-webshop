5f8b3b2b38611613e8e66d52934d44cc40ec5c0a
Patrick Minogue <patrick.minogue@gmail.com>
2018-05-30 15:31:04 +0200

    Added bokain_consultation sections

--- lib/app_component.dart
+++ lib/app_component.dart
@@ -2,6 +2,7 @@ import 'package:angular/angular.dart';
 import 'package:angular_components/angular_components.dart';
 import 'package:angular_router/angular_router.dart';
 import 'package:bokain_models/bokain_models.dart';
+import 'package:fo_components/fo_components.dart';
 import 'frontpage_component/frontpage_component.template.dart'
     as frontpage_comp;
 import 'route_paths.dart' as route_paths;
@@ -19,20 +20,32 @@ import 'skin_consultation_component/skin_consultation_component.template.dart'
   ],
   providers: const [
     materialProviders,
+    PhraseService,
+    PhrasesService,
     ProductService,
     UserService,
     routerProvidersHash
   ],
 )
 class AppComponent {
-  AppComponent(this.productService, this.userService) {
+  AppComponent(this.phrasesService, this.productService, this.userService) {
+    
     userService
         .login('patrick.minogue@gmail.com', 'lok13rum')
-        .then((_) => productService.streamMany());
+        .then((_) {
+          phrasesService.fetchAll('sv').then((value) {     
+            PhraseService.language = 'sv';       
+            PhraseService.data = value;
+            productService.streamMany();
+
+            loaded = true;
+          });
+        });
     //dynamicPhraseService.streamLanguage('sv');
   }
-  ProductService productService;
-  UserService userService;
+  final PhrasesService phrasesService;
+  final ProductService productService;
+  final UserService userService;
 
 
   final List<RouteDefinition> routes = [
@@ -43,4 +56,6 @@ class AppComponent {
         routePath: route_paths.skinConsultation,
         component: consultation_comp.SkinConsultationComponentNgFactory)
   ];
+
+  bool loaded = false;
 }
--- lib/skin_consultation_component/skin_consultation_component.dart
+++ lib/skin_consultation_component/skin_consultation_component.dart
@@ -1,13 +1,48 @@
+import 'dart:async';
 import 'package:angular/angular.dart';
+import 'package:angular_components/angular_components.dart';
 import 'package:bokain_consultation/bokain_consultation.dart';
+import 'package:bokain_models/bokain_models.dart';
+import 'package:fo_components/fo_components.dart';
 
 @Component(
-  selector: 'skin-consultation-component',
-  templateUrl: 'skin_consultation_component.html',
-  styleUrls: const ['skin_consultation_component.css'],
-  directives: const [],
-  providers: const [])
-class SkinConsultationComponent {
-
+    selector: 'skin-consultation-component',
+    templateUrl: 'skin_consultation_component.html',
+    styleUrls: const [
+      'skin_consultation_component.css'
+    ],
+    directives: const [
+      ButtonCloudSingleSelectComponent,
+      ConsultationSectionPicturesComponent,
+      ConsultationSectionProblemAreasComponent,
+      ConsultationSectionSymptomsComponent,
+      coreDirectives,
+      materialDirectives,
+    ],
+    providers: const [
+      ConsultationService,
+      CustomerService
+    ],
+    pipes: const [
+      PhrasePipe
+    ])
+class SkinConsultationComponent { 
   SkinConsultationComponent();
+
+  Future onCreateCustomer() async {
+    customer.id = 'hej';
+  }
+
+  Date get birthdate => customer.birthdate == null ? null : Date.fromTime(customer.birthdate);
+
+  set birthdate(Date date) {
+    customer.birthdate = date == null ? null : new DateTime(date.year, date.month, date.day);
+  }
+
+  Customer customer = new Customer();
+  Consultation consultation = new Consultation();
+  final Date minDate = new Date.today().add(years: -100);
+  final Date maxDate = new Date.today().add(years: -10);
+
+  bool termsAccepted = false;
 }
