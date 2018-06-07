864449bdf397c3e4d919ee27cd8077d507ead805
Patrick Minogue <patrick.minogue@gmail.com>
2018-06-07 10:37:34 +0200

    Moved a bunch of files around

--- lib/app_component.dart
+++ lib/app_component.dart
@@ -2,50 +2,33 @@ import 'package:angular/angular.dart';
 import 'package:angular_components/angular_components.dart';
 import 'package:angular_router/angular_router.dart';
 import 'package:bokain_models/bokain_models.dart';
-import 'package:fo_components/fo_components.dart';
-import 'frontpage_component/frontpage_component.template.dart'
+import 'components/frontpage_component/frontpage_component.template.dart'
     as frontpage_comp;
-import 'route_paths.dart' as route_paths;
-import 'skin_consultation_component/skin_consultation_component.template.dart'
+import 'components/skin_consultation_component/skin_consultation_component.template.dart'
     as consultation_comp;
+import 'route_paths.dart' as route_paths;
 
 @Component(
   selector: 'my-app',
   styleUrls: const ['app_component.css'],
   templateUrl: 'app_component.html',
-  directives: const [
-    materialDirectives,    
-    coreDirectives,
-    routerDirectives
-  ],
+  directives: const [materialDirectives, coreDirectives, routerDirectives],
   providers: const [
     materialProviders,
-    PhraseService,
-    PhrasesService,
+    MessagesService,        
     ProductService,
     UserService,
     routerProvidersHash
   ],
+  pipes: const []
 )
 class AppComponent {
-  AppComponent(this.phrasesService, this.productService, this.userService) {
-    
-    userService
-        .login('patrick.minogue@gmail.com', 'lok13rum')
-        .then((_) {
-          phrasesService.fetchAll('sv').then((value) {     
-            PhraseService.language = 'sv';       
-            PhraseService.data = value;
-            productService.streamMany();
-
-            loaded = true;
-          });
-        });
-  }
-  final PhrasesService phrasesService;
+  AppComponent(this.productService, this.userService, this.msg) {
+    userService.login('patrick.minogue@gmail.com', 'lok13rum').then((_) => loaded = true);    
+  }  
   final ProductService productService;
   final UserService userService;
-
+  final MessagesService msg;
 
   final List<RouteDefinition> routes = [
     new RouteDefinition(
