0e9079aa5e5e39901853a64e7682572e395edf1d
Patrick Minogue <patrick.minogue@gmail.com>
2018-06-26 15:55:02 +0200

    shuffled member order in frontpageComponent

--- lib/components/frontpage_component/frontpage_component.dart
+++ lib/components/frontpage_component/frontpage_component.dart
@@ -3,16 +3,14 @@ import 'package:bokain_models/bokain_models.dart';
 import '../productbox_component/productbox_component.dart';
 
 @Component(
-  selector: 'frontpage-component',
-  templateUrl: 'frontpage_component.html',
-  styleUrls: const ['frontpage_component.css'],
-  directives: const [coreDirectives, ProductBoxComponent],
-  providers: const [])
+    selector: 'frontpage-component',
+    templateUrl: 'frontpage_component.html',
+    styleUrls: const ['frontpage_component.css'],
+    directives: const [coreDirectives, ProductBoxComponent],
+    providers: const [])
 class FrontpageComponent {
-
-  FrontpageComponent(this.productService);
-
-  final ProductService productService;
+  FrontpageComponent(this.productService);  
 
   String selectedProductId;
+  final ProductService productService;
 }
