c5d1c688de0e447b4225a123e6cf821ad9a9e9bb
Patrick Minogue <patrick.minogue@gmail.com>
2018-06-26 15:56:11 +0200

    Made ProductBoxComponent use product's internal lang messages

--- lib/components/productbox_component/productbox_component.dart
+++ lib/components/productbox_component/productbox_component.dart
@@ -2,34 +2,21 @@ import 'dart:async';
 import 'package:angular/angular.dart';
 import 'package:angular_components/angular_components.dart';
 import 'package:bokain_models/bokain_models.dart';
+import 'package:intl/intl.dart';
 
 @Component(
     selector: 'bo-productbox',
     styleUrls: const ['productbox_component.css'],
     templateUrl: 'productbox_component.html',
-    directives: const [coreDirectives, materialDirectives],
-    providers: const [LanguageService],
+    directives: const [materialDirectives],
+    providers: const [],
     pipes: const [],
     changeDetection: ChangeDetectionStrategy.OnPush)
 class ProductBoxComponent implements OnInit, OnDestroy {
-  ProductBoxComponent(this._phrasesService, this._languageService);
+  ProductBoxComponent();
 
   @override
-  void ngOnInit() {
-    final langId = _languageService.data.keys.firstWhere(
-        (id) => _languageService.data[id].iso639_1 == 'sv');
-    if (model == null ||
-        _phrasesService.customData['products'][model.id] == null) {
-      phrasesModel = new ProductPhrases();
-    } else {
-      final custom =
-          new Map.from(_phrasesService.customData['products'][model.id])
-              .cast<String, String>()
-                ..removeWhere((key, value) => !key.endsWith('_$langId'));
-
-      phrasesModel = new ProductPhrases.fromJson(custom.map((key, value) =>
-          new MapEntry(key.substring(0, key.indexOf('_$langId')), value)));
-    }
+  void ngOnInit() {        
   }
 
   @override
@@ -37,16 +24,13 @@ class ProductBoxComponent implements OnInit, OnDestroy {
     addButtonClickController.close();
   }
 
-  ProductPhrases phrasesModel;
+  String get lang => Intl.shortLocale(Intl.getCurrentLocale());
 
   @Input('model')
   Product model;
 
   @Output('addButtonClick')
   Stream<String> get addButtonClickOutput => addButtonClickController.stream;
-
-  final LanguageService _languageService;
-  final PhrasesService _phrasesService;
 
   final StreamController<String> addButtonClickController =
       new StreamController();
