ece7cd7f57ec97f1fcd8546031c657268d837fd6
Patrick Minogue <patrick.minogue@gmail.com>
2018-06-27 12:08:04 +0200

    Added SkinTypeFilterComponent and its routes

---
+++ lib/components/skin_type_filter_component/skin_type_filter_component.dart
@@ -0,0 +1,12 @@
+import 'package:angular/angular.dart';
+
+@Component(
+  selector: 'bo-skin-type-filter',
+  templateUrl: 'skin_type_filter_component.html',
+  styleUrls: const ['skin_type_filter_component.css']
+  directives: const [],
+  providers: const []
+)
+class SkinTypeFilterComponent {
+  
+}
