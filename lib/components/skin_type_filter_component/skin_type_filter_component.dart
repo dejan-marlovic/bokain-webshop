96de933bdb626326edc9c978398cdfff876629f7
Patrick Minogue <patrick.minogue@gmail.com>
2018-06-28 10:24:07 +0200

    SkinTypeFilter now lists products

--- lib/components/skin_type_filter_component/skin_type_filter_component.dart
+++ lib/components/skin_type_filter_component/skin_type_filter_component.dart
@@ -3,30 +3,32 @@ import 'package:angular_router/angular_router.dart';
 import 'package:bokain_models/bokain_models.dart';
 import '../productbox_component/productbox_component.dart';
 
-
 @Component(
     selector: 'bo-skin-type-filter',
     templateUrl: 'skin_type_filter_component.html',
     styleUrls: const ['skin_type_filter_component.css'],
     directives: const [NgFor, ProductBoxComponent])
 class SkinTypeFilterComponent implements OnActivate {
-
-  SkinTypeFilterComponent(this._productService);
-
-  /*
-products = _productService.cachedModels.values.where((product) =>
-            product.skin_type_ids
-                .contains(skinType)).toList(growable: false);
-*/
-  
+  SkinTypeFilterComponent(this._productService, this._skinTypeService);
 
   @override
   void onActivate(RouterState previous, RouterState current) {
-    print(current.path);
+    if (current.parameters['id'] != null) {
+      final skinTypeId = _skinTypeService.data.keys.firstWhere(
+          (id) =>
+              _skinTypeService.data[id].url_name == current.parameters['id'],
+          orElse: () => null);
 
+      if (skinTypeId != null) {
+        products = _productService.cachedModels.values
+            .where((product) => product.skin_type_ids.contains(skinTypeId))
+            .toList(growable: false);
+      }
+    }
   }
 
   List<Product> products = [];
   String selectedProductId;
   final ProductService _productService;
+  final SkinTypeService _skinTypeService;
 }
