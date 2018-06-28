0b8b23ea45bbaad3cebb76c1432cad540cd54e18
Patrick Minogue <patrick.minogue@gmail.com>
2018-06-28 11:15:06 +0200

    Product category filter now lists products

--- lib/components/product_category_filter_component/product_category_filter_component.dart
+++ lib/components/product_category_filter_component/product_category_filter_component.dart
@@ -1,34 +1,32 @@
 import 'package:angular/angular.dart';
 import 'package:angular_router/angular_router.dart';
 import 'package:bokain_models/bokain_models.dart';
+import 'package:intl/intl.dart';
 
 @Component(
-  selector: 'bo-product-category-filter',
-  templateUrl: 'product_category_filter_component.html',
-  styleUrls: const ['product_category_filter_component.css'],
-  directives: const [NgFor],
-  providers: const []
-)
+    selector: 'bo-product-category-filter',
+    templateUrl: 'product_category_filter_component.html',
+    styleUrls: const ['product_category_filter_component.css'],
+    directives: const [NgFor],
+    providers: const [])
 class ProductCategoryFilterComponent implements OnActivate {
-  ProductCategoryFilterComponent(this._productCategoryService, this._productService);
-  
+  ProductCategoryFilterComponent(
+      this._productCategoryService, this._productService);
+
   @override
   void onActivate(RouterState previous, RouterState current) {
     if (current.parameters['id'] != null) {
-      //_productCategoryService.cachedModels.values.first.name
-
-      /*
-      final skinTypeId = _skinTypeService.data.keys.firstWhere(
-          (id) =>
-              _skinTypeService.data[id].url_name == current.parameters['id'],
+      final lang = Intl.shortLocale(Intl.getCurrentLocale());
+      final category = _productCategoryService.cachedModels.values.firstWhere(
+          (category) =>
+              category.phrases[lang].url_name == current.parameters['id'],
           orElse: () => null);
-1
-      if (skinTypeId != null) {
-        products = _productService.cachedModels.values
-            .where((product) => product.skin_type_ids.contains(skinTypeId))
-            .toList(growable: false);
-      }*/
 
+      if (category != null) {
+        products = _productService.cachedModels.values
+            .where((product) => product.product_category_id == product.id)
+            .toList(growable: false);
+      }
     }
   }
 
