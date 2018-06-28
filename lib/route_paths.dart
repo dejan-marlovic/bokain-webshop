6d22cce99245eb8db0cc105550ba4ccdd5c84752
Patrick Minogue <patrick.minogue@gmail.com>
2018-06-28 11:16:50 +0200

    Added id parameter to ProductCategory route

--- lib/route_paths.dart
+++ lib/route_paths.dart
@@ -7,10 +7,11 @@ final RoutePath frontpage =
     new RoutePath(path: 'index.html', useAsDefault: true);
 
 final RoutePath productCategoryFilter = new RoutePath(
+    // ignore: prefer_interpolation_to_compose_strings    
     path: url(Intl.plural(2,
         one: 'product category',
         other: 'product categories',
-        name: 'product category')));
+        name: 'product category')) + '/:id');
 
 final RoutePath skinConsultation = new RoutePath(
     path: url(Intl.plural(1,
