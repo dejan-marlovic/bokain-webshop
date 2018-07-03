c8fb2a1ccea1c88c8a6bee09dee673990c799dbb
Patrick Minogue <patrick.minogue@gmail.com>
2018-07-03 08:18:06 +0200

    Added tabs to ProfileComponent

--- lib/components/profile_component/profile_component.dart
+++ lib/components/profile_component/profile_component.dart
@@ -1,13 +1,16 @@
 import 'package:angular/angular.dart';
+import 'package:bokain_models/bokain_models.dart';
+import 'package:fo_components/fo_components.dart';
 
 @Component(
-  selector: 'bo-profile',
-  templateUrl: 'profile_component.html',
-  styleUrls: const ['profile_component.css'],
-  directives: const [NgIf]
-)
+    selector: 'bo-profile',
+    templateUrl: 'profile_component.html',
+    styleUrls: const ['profile_component.css'],
+    directives: const [FoTabComponent, FoTabPanelComponent, NgIf])
 class ProfileComponent {
-
+  ProfileComponent(this.msg);
 
   bool loggedIn = true;
+
+  final MessagesService msg;
 }
