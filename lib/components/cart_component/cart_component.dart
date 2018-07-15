e1066606260fda8abaddca06d5a748f2966e04d2
Patrick Minogue <patrick.minogue@gmail.com>
2018-07-15 23:15:47 +0200

    Added discount input

--- lib/components/cart_component/cart_component.dart
+++ lib/components/cart_component/cart_component.dart
@@ -1,5 +1,6 @@
 import 'package:angular/angular.dart';
-//import 'package:angular_components/angular_components.dart';
+import 'package:angular_components/angular_components.dart';
+import 'package:angular_forms/angular_forms.dart';
 import 'package:bokain_models/bokain_models.dart';
 import 'package:fo_components/fo_components.dart';
 import '../../services/cart_service.dart';
@@ -9,34 +10,23 @@ import 'cart_item_component.dart';
     selector: 'bo-cart',
     templateUrl: 'cart_component.html',
     styleUrls: const ['cart_component.css'],
-    directives: const [CartItemComponent, NgFor],
+    directives: const [CartItemComponent, formDirectives, materialInputDirectives, NgFor],
     pipes: const [NamePipe])
 class CartComponent {
   CartComponent(this.cartService, this.productService, this.msg);
 
-  int get subtotal {
+  double get subtotal {
     var output = 0.0;
     for (final productId in cartService.productRegistry.keys) {
       final count = cartService.productRegistry[productId];
       final product = productService.get(productId);
-
-      switch (cartService.currency) {
-        case Currency.eur:
-          output += product.price_eur;
-          break;
-
-        case Currency.sek:
-          output += product.price_sek;
-          break;
-
-        case Currency.usd:
-          output += product.price_usd;
-          break;
-      }
+      output += product.price[cartService.currency] * count;
     }
 
-    return 0;
+    return output;
   }
+
+  String discountCode;
 
   final CartService cartService;
   final ProductService productService;
