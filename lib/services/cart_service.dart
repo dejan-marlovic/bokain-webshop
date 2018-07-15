9a3ca344662b88927318713306a779278c9552c5
Patrick Minogue <patrick.minogue@gmail.com>
2018-07-15 23:16:48 +0200

    cart service fix

--- lib/services/cart_service.dart
+++ lib/services/cart_service.dart
@@ -25,6 +25,6 @@ class CartService {
   // Key: Product id Value: number of products
   final Map<String, int> productRegistry = {};
 
-  final Currency currency = Currency.sek;
+  final String currency = 'sek';
 }
 
