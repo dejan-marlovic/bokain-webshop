46cc2a1650f931b3b50192880f755c4a77b8dbf5
Patrick Minogue <patrick.minogue@gmail.com>
2018-08-03 10:26:27 +0200

    Created ProductCategoryComponent

---
+++ lib/components/product_category_component/product_category_component.dart
@@ -0,0 +1,31 @@
+import 'package:angular/angular.dart';
+import 'package:fo_components/fo_components.dart';
+import 'package:bokain_models/bokain_models.dart';
+import '../productbox_component/productbox_component.dart';
+
+@Component(
+  selector: 'bo-product-category',
+  templateUrl: 'product_category_component.html',
+  styleUrls: const ['product_category_component.css'],
+  directives: const [NgFor, NgIf, ProductBoxComponent],
+  providers: const [],
+  pipes: const [NamePipe]
+)
+class ProductCategoryComponent implements OnInit {
+
+  ProductCategoryComponent(this.languageService, this._productService);
+
+  @override
+  void ngOnInit() {
+    products = _productService.cachedModels.values
+            .where((product) => product.product_category_id == model.id);
+  }
+
+  Iterable<Product> products;
+  String selectedProductId;
+  final LanguageService languageService;
+  final ProductService _productService;
+
+  @Input()
+  ProductCategory model;
+}
--- lib/components/product_category_filter_component/product_category_filter_component.dart
+++ lib/components/product_category_filter_component/product_category_filter_component.dart
@@ -4,40 +4,45 @@ import 'package:bokain_models/bokain_models.dart';
 import 'package:fo_components/fo_components.dart';
 import 'package:intl/intl.dart';
 import '../icon_component/icon_component.dart';
-import '../productbox_component/productbox_component.dart';
+import '../product_category_component/product_category_component.dart';
 
 @Component(
     selector: 'bo-product-category-filter',
     templateUrl: 'product_category_filter_component.html',
-    styleUrls: const ['product_category_filter_component.css'],
-    directives: const [IconComponent, NgFor, ProductBoxComponent, routerDirectives],
+    styleUrls: const [
+      'product_category_filter_component.css'
+    ],
+    directives: const [
+      IconComponent,
+      NgFor,
+      NgIf,      
+      ProductCategoryComponent,
+      routerDirectives
+    ],
     providers: const [],
-    pipes: const [NamePipe])
+    pipes: const [      
+      NamePipe
+    ])
 class ProductCategoryFilterComponent implements OnActivate {
   ProductCategoryFilterComponent(
-      this._productCategoryService, this._productService, this.msg);
+      this.languageService, this._productCategoryService, this.msg)
+      : categories = _productCategoryService.cachedModels.values;
 
   @override
   void onActivate(RouterState previous, RouterState current) {
-    if (current.parameters['id'] != null) {      
-      final lang = Intl.shortLocale(Intl.getCurrentLocale()).toUpperCase();      
-      final category = _productCategoryService.cachedModels.values.firstWhere(
+    if (current.parameters['id'] != null) {
+      final lang = Intl.shortLocale(Intl.getCurrentLocale()).toUpperCase();
+      category = _productCategoryService.cachedModels.values.firstWhere(
           (category) =>
               category.phrases[lang].url_name == current.parameters['id'],
           orElse: () => null);
-
-      if (category != null) {
-        products = _productService.cachedModels.values
-            .where((product) => product.product_category_id == category.id)
-            .toList(growable: false);
-      }
     }
   }
 
+  ProductCategory category;
   String iconSize = '2.5rem';
-  List<Product> products = [];
-  String selectedProductId;
+  final Iterable<ProductCategory> categories;
+  final LanguageService languageService;
   final ProductCategoryService _productCategoryService;
-  final ProductService _productService;
   final WebshopMessagesService msg;
 }
